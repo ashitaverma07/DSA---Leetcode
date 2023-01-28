6bc3798aefa224f909d574721e24ed81ba03e62e
Ashita Verma <ashitaverma14@gmail.com>
2023-01-29 00:44:04 +0530

    Time: 1 ms (99.90%), Space: 46 MB (69.19%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majorityElement = nums[0];
+    int count = 0;
+    for (int num : nums) {
+      if (count == 0) {
+        majorityElement = num;
+      }
+      count += num == majorityElement ? 1 : -1;
+    }
+    return majorityElement;
+        
+    }
+}
