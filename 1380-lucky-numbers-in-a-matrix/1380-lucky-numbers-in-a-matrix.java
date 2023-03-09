e452e2369a56bc1094fe0aef3a7df85b86ac9284
Ashita Verma <ashitaverma14@gmail.com>
2023-03-09 23:55:20 +0530

    Time: 2 ms (63.15%), Space: 42.8 MB (74.29%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] < matrix[i][row[i]]) {
+                    row[i] = j;
+                }
+
+                if (matrix[i][j] > matrix[col[j]][j]) {
+                    col[j] = i;
+                }
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            int j = row[i];
+            if (col[j] == i) {
+                res.add(matrix[i][j]);
+            }
+        }
+
+        return res;
+    }
+}
